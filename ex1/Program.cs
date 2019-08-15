eac10598899b3b8f2caa1f29a55936a73ebac857
tzahibe <tzachi222@gmail.com>
2019-08-15 15:42:26 +0300

    Add files via upload

---
+++ ex1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+using System.Collections.Concurrent;
+
+namespace ex1
+{
+    public class Program
+    {
+        static readonly int CASHIER_NUM = 5;
+        static readonly object[] cashiers = new object[CASHIER_NUM];
+        static ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+        static Random rnd = new Random();
+
+
+        static void ProduceCustomers()
+        {
+            int count = 0;
+            while(true)
+            {
+                string threadName = "Customer " + (count + 1);
+                Console.WriteLine(threadName + " is Enter to the Queue!");
+                queue.Enqueue(threadName);
+                Thread.Sleep(1000);
+                count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread producingThread = new Thread(ProduceCustomers);
+            producingThread.Start();
+
+            for (int k = 0; k < CASHIER_NUM; k++)
+            {
+                cashiers[k] = new object();
+            }
+
+            for (int i = 0; i < CASHIER_NUM; i++)
+            {
+                int cashierNum = i;
+                Thread cashierThread = new Thread(() => FindFreeCashier(cashierNum));
+                cashierThread.Start();
+            }
+
+
+            //int customerCount = 0;
+
+            //while (CUSTOMERS > customerCount)
+            //{
+            //    string threadName = "Customer " + (customerCount + 1);
+            //    ThreadStart start = new ThreadStart(() => FindFreeCashier(threadName));
+            //    Thread thr = new Thread(start);
+            //    thr.Name = threadName;
+            //    Monitor.Enter(queue);
+            //    queue.Enqueue(thr);
+            //    Console.WriteLine(threadName + " is waiting in the Queue");
+            //    Monitor.Exit(queue);
+            //    thr.Start();
+            //    Thread.Sleep(1000);
+
+            //    customerCount++;
+            //}
+
+            //Console.ReadKey();
+        }
+        public static void FindFreeCashier(int cashieNum)
+        {
+            string customerName = string.Empty;
+
+            while (true)
+            {
+                if (queue.TryDequeue(out customerName))
+                {
+                    Console.WriteLine("Queue of: " + customerName);
+                    Checkout_Cashier(customerName, cashieNum);
+                }
+            }
+        }
+
+        public static void Checkout_Cashier(string customerName, int cashieNum)
+        {
+            int randomNum = rnd.Next(1, 5);
+            Console.WriteLine(customerName + " is Getting service in cashir number " + (cashieNum + 1));
+            Console.WriteLine("Service " + (cashieNum + 1) + " will be busy for the next " + randomNum * 1000 + " seconds!");
+            Thread.Sleep(randomNum * 1000);
+            Console.WriteLine(customerName + " is Done in Service " + (cashieNum + 1) + "!");
+        }
+    }
+}
---
+++ ex2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    public class Program
+    {
+        static int length = 5;
+
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string line;
+            string input;
+            List<string> result = new List<string>();
+
+            while (true)
+            {
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(Directory.GetCurrentDirectory() + "/data.txt");
+
+                Console.WriteLine("Please Insert 5 letters"); 
+                input = Console.ReadLine();
+                Console.WriteLine("input = " + input);
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (IsInputEqualLine(line, input))
+                    {
+                        result.Add(line);
+                    }
+                    counter++;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Find " + result.Count + " Results");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                foreach (string i in result)
+                {
+                    Console.Write("{0}\t", i.ToString());
+                }
+                Console.WriteLine("\n\n");
+                file.Close();
+                result.Clear();
+            }
+        }
+
+        public static bool IsInputEqualLine(string line, string input)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (line[i] == input[j])
+                    {
+                        input = input.Remove(j, 1);
+                        input = input.Insert(j, "*");
+                        line = line.Remove(i, 1);
+                        line = line.Insert(i, "*");
+                    }
+                }
+            }
+
+            if (line.Equals(input))
+                return true;
+
+            return false;
+        }
+    }
+}
